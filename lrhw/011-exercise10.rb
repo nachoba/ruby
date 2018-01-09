e6ae7df74ac1567ea0e336e2efbd3691c2c8d4e5
nachoba <0800nacho@gmail.com>
2018-01-09 20:57:04 -0300

    more exercises

--- lrhw/011-exercise10.rb
+++ lrhw/011-exercise10.rb
@@ -24,3 +24,58 @@
 #
 #   		"I am 6'2\" tall."			# escape double-quote inside string
 #   		'I am 6\'2" tall.'			# escape single-quote inside string
+#
+#  The second way is by using  triple-quotes, which is just """ and works like a
+#  string, but you can also put as many lines of text as you want until you type
+#  """ again. We'll also play with these.
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+
+#  Escape Sequences
+#  ----------------
+#  This is a ll of the escape  sequences Ruby supports.  You may not use many of
+#  these,but memorize their format and what they do anyway. Try them out in some
+#  strings to see if you can make them work.
+#
+# 		Escape 					What it Does
+#  		----------				-----------------------
+# 		\\						Backslash (\)
+# 		\'						Single-Quote (')
+#   	\"						Double-Quote (")
+#  		\a 						ASCII bel (BEL)
+#  		\b 						ASCII backspace (BS)
+#  		\f     					ASCII formfeed (FF)
+#    	\n 						ASCII linefeed (LF)
+#  		\r 						ASCII carriage Return (CR)
+#  		\t 						ASCII horizontal Tab (TAB)
+#  		\uxxxx  				Character with 16-bit hex value (Unicode only)
+#   	\v   					ASCII vertical tab (VT)
+#  		\ooo 					Character with octal value ooo
+#      	\xhh 					Character with hex value hh
+#
+#
+#  Study Drills
+#  ------------
+#  [1] Memorize all the escape sequences by putting them on flash cards.
+#  [2] Use ''' (triple-single-quote) instead. Can you see why you might use that
+#      instead of """?
+#  [3] Combine escape sequences and format strings to create a more complex for-
+#      mat.
+
+check_mark = "🦄"
+puts check_mark
